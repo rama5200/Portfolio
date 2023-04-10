eb62801cf0fadff5aeb41ed346b3422194f71af9
rama5200 <ramaslully@gmail.com>
2023-04-10 11:35:30 -0300

    Add gitignore and fix profile photo

--- src/components/PortfolioTitle.js
+++ src/components/PortfolioTitle.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
+import profileImage from '../profile.jpeg'; // Asegúrate de que la ruta sea correcta
+
 
 const PortfolioTitle = () => {
   return (
     <Box sx={{ textAlign: 'center', backgroundColor: '#007F8D', py: 5, display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
       <Box sx={{ borderRadius: '50%', overflow: 'hidden', marginRight: 3, flexShrink: 0, width: '150px', height: '150px', marginLeft: '30px' }}>
-        <img src="profile.jpeg" alt="Mi foto de perfil" width="100%" height="100%" style={{ objectFit: 'cover' }} />
+        <img src={profileImage} alt="Mi foto de perfil" width="100%" height="100%" style={{ objectFit: 'cover' }} />
       </Box>
       <Box sx={{ alignSelf: 'flex-start' }}>
         <Typography variant="h3" component="h1" sx={{ color: '#fff', fontFamily: 'Roboto', fontWeight: 700, marginBottom: 1 }}>
